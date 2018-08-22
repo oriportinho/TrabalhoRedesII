ce010e4722e6d557d77d54f26b0a835e355c6dfd
oriportinho <ori.portinho@gmail.com>
2018-08-22 13:32:07 -0300

    criacao de chave de cripografia

--- TrabalhoRedesII/TrabalhoRedesII.py
+++ TrabalhoRedesII/TrabalhoRedesII.py
@@ -1,1 +1,25 @@
+import secrets
+import json
 
+
+def CriarChave():
+    a = secrets.token_bytes(380)
+    arq = open('chave.txt', 'w')
+    chave = []
+
+    for i in a:
+        chave.append(str(i) + "\n")
+
+    arq.writelines(chave)
+    arq.close()
+
+
+
+#CriarChave()
+
+eleicao = open('eleicao.json', 'r')
+
+votos = json.load(eleicao)
+print(votos)
+
+v = zeros(127, Int)
